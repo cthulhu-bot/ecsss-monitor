90baafdd0c5ef5bbf1879f48b30e4c0436e9cb92
Joshua Stupplebeen <jpstup@gmail.com>
2013-04-08 23:53:54 -0600

    Added add/update/delete page logic to mongo

--- routes/pages.js
+++ routes/pages.js
@@ -36,3 +36,50 @@ exports.findById = function(req, res) {
         });
     });
 };
+
+exports.addPage = function(req, res) {
+    var page = req.body;
+    console.log('Adding page: ' + JSON.stringify(page));
+    db.collection('pages', function(err, collection) {
+        collection.insert(page, {safe:true}, function(err, collection) {
+            if (err) {
+                res.send({'error':'An error has occurred'});
+            } else {
+                console.log('Success: ' + JSON.stringify(result[0]));
+                res.send(result[0]);
+        });
+    });
+}
+
+exports.updatePage = function(req, res) {
+    var id = req.params.id;
+    var page = req.body;
+    console.log('Updating  page: ' + id);
+    console.log(JSON.stringify(page));
+    db.collection('pages', function(err, collection) {
+        collection.update({'_id':new BSON.ObjectID(id)}, page, {safe:true}, function(err, result) {
+            if (err) {
+                console.log('Error updating wine: ' + err);
+                res.send({'error':'An error has occurred'});
+            } else {
+                console.log('' + result + ' document(s) updated');
+                res.send(page);
+            }
+        });
+    });
+}
+
+exports.deletePage = function(req, res) {
+    var id = req.params.id;
+    console.log('Deleting page: ' + id);
+    db.collection('pages', function(err, collection) {
+        collection.remove({'_id':new BSON.ObjectID(id)}, {safe:true}, function(err, result) {
+            if (err) {
+                res.send({'error':'An error has occurred'});
+            } else {
+                console.log('' + result + ' document(s) deleted');
+                res.send(req.body);
+            }
+        });
+    });
+}
